2c09ae7a3c3139462e1c9a2a7dca8b786eb77802
= <=>
2012-11-21 09:23:54 +0800

    model_app

---
+++ aa/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aa::Application.config.secret_token = '9bacdf477b45d64675d0104a98847f1081f7b4374384b3aca1d4e3b30ec1595c2f1b8ecc6df1489b142d310adff6d95cf593351d9ab1f774695025dda6d5ad9b'
